7414cc9834fb8f26dc4d29d0f4519cf59c88b85c
safestone <zjaehf02@naver.com>
2026-06-05 02:44:40 +0900

    rotate 구현 중 잦은 에러 발생 및 구조가 복잡해져서 기존에 쓰던 것을 버리고 다시 제작.
    1. rotation 변수 선언
    2. getTransformedShape()를 통해 transform된 도형의 Bondry를 얻어냄.
    3. draw, contains를 transform된 boundry를 받아서 처리하도록 수정.

--- src/main/java/Shapes/GShape.java
+++ src/main/java/Shapes/GShape.java
@@ -3,10 +3,13 @@ package Shapes;
 import Global.EAnchor;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public abstract class GShape implements Cloneable{
     protected Shape geometry;
+    protected double rotation = 45;
     private GAnchor anchor;
+
 
     public GShape() {
         anchor = new GAnchor();
@@ -21,26 +24,40 @@ public abstract class GShape implements Cloneable{
         }
     }
     public void draw(Graphics2D graphics2D) {
-        graphics2D.draw(geometry);
+        graphics2D.draw(getTransformedShape());
     }
     public boolean getContains(int x, int y) {
-        return geometry.contains(x, y);
+        return getTransformedShape().contains(x, y);
     }
 
     public EAnchor getAnchor(int x, int y) {
         return anchor.getAnchor(x, y);
     }
     public void drawSelected(Graphics2D g2d){
-        Rectangle bounds = geometry.getBounds();
+        Rectangle bounds = getTransformedShape().getBounds();
         g2d.draw(bounds);
 
         anchor.setPosition(bounds);
         anchor.draw(g2d);
     }
+    public Shape getTransformedShape() {
+        Rectangle bounds = geometry.getBounds();
 
+        double cx = bounds.getX();
+        double cy = bounds.getY();
+
+        AffineTransform at = new AffineTransform();
+        at.rotate(Math.toRadians(rotation), cx, cy);
+
+        return at.createTransformedShape(geometry);
+    }
     public abstract void setStart(int x, int y);
     public abstract void setEnd(int x, int y);
     public void setCont(int x, int y){};
     public abstract void transfer(int dx, int dy);
     public abstract void resize(EAnchor anchor, int dx, int dy);
+
+    public void rotate(double angle) {
+        rotation += angle;
+    }
 }
